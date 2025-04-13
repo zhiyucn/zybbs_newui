727883054dd71a22a761e83ccc7bc57e29bedf8d
zhiyucn <zhiyuxl@outlook.com>
2025-04-13 14:59:32 +0800

    fix: 更新开发服务器主域名

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ const { all } = require("core-js/features/promise");
 module.exports = {
   devServer: {
     port : 2398,
+    // 修改主域名
+    host: 'newui.bbs.zhiyuhub.top',
     allowedHosts: ['newui.bbs.zhiyuhub.top'],
     proxy: {
       '/api': {
