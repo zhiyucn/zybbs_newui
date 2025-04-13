f384ec3161a59c1f3ec7bd2bf3f830c2b1e7309b
zhiyucn <zhiyuxl@outlook.com>
2025-04-13 14:56:42 +0800

    style: 增加开发服务器端口配置

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { all } = require("core-js/features/promise");
 
 module.exports = {
   devServer: {
+    port : 2398,
     allowedHosts: ['newui.bbs.zhiyuhub.top'],
     proxy: {
       '/api': {
