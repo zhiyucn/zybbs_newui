59652022153ee68cdc48d171781b41c4f173044f
zhiyucn <zhiyuxl@outlook.com>
2025-04-13 14:09:20 +0800

    fix: 添加allowedHosts配置以允许特定主机

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
+const { all } = require("core-js/fn/promise");
+
 module.exports = {
   devServer: {
+    allowedHosts: ['newui.bbs.zhiyuhub.top'],
     proxy: {
       '/api': {
         target: 'https://bbs.zhiyuhub.top',
